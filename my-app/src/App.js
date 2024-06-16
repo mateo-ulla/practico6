04a94c3047b82096913a3dc7ae85401b1a000d65
Mateo Ulla <mateoulla1@gmail.com>
2024-06-16 11:01:26 -0300

    Primer commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Materias ciclo lectivo 2023</h3>
+        <ul>
+          <li>Matemática</li>
+          <li>Lengua</li>
+          <li>Programación II</li>
+          <li>Física</li>
+          <li>Informática Aplicada II</li>
+          <li>Química</li>
+          <li>Robótica</li>
+        </ul>
       </header>
     </div>
   );
 }
-
 export default App;
