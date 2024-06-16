91e9f648dd253dbba3d8ccfc4852f7516d392c16
Mateo Ulla <mateoulla1@gmail.com>
2024-06-16 11:27:17 -0300

    Ejercicio 1, Pagina 5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,21 +2,60 @@ import React from "react";
 import "./App.css";
 function App() {
   const subjects = [
-    "Matemática",
-    "Lengua",
-    "Programación II",
-    "Física",
-    "Informática Aplicada II",
-    "Química",
-    "Robótica",
+    {
+      name: "Matemática",
+      teacher: "Carlos González",
+      marks: [8, 9, 7, 2],
+      year: 2022,
+      id: 0,
+    },
+    {
+      name: "Lengua",
+      teacher: "María Rodríguez",
+      marks: [9, 10, 8],
+      year: 2021,
+      id: 1,
+    },
+    {
+      name: "Programación II",
+      teacher: "Ana Martínez",
+      marks: [10, 9, 10],
+      year: 2023,
+      id: 2,
+    },
+    {
+      name: "Física",
+      teacher: "Luis Fernández",
+      marks: [6, 8, 7, 9],
+      year: 2020,
+      id: 4,
+    },
+    {
+      name: "Informática Aplicada II",
+      teacher: "Jorge Pérez",
+      marks: [9, 8, 9],
+      year: 2024,
+      id: 5,
+    },
+    {
+      name: "Química",
+      teacher: "Elena Sánchez",
+      marks: [7, 8, 7, 6],
+      year: 2019,
+      id: 6,
+    },
   ];
+  const filteredSubjects = subjects.filter(
+    (subject) => subject.marks.length > 3
+  );
+
   return (
     <div>
       <header className="App-header">
         <h3>Materias ciclo lectivo 2023</h3>
         <ul>
-          {subjects.map((subject) => (
-            <li>{subject}</li>
+          {filteredSubjects.map((subject) => (
+            <li key={subject.id}>{subject.name}</li>
           ))}
         </ul>
       </header>
